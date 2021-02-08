bf798250e906047cf7e1cc8e407fdb891d73b557
rbauer420 <rachel.bauer1@gmail.com>
2021-02-08 17:58:01 -0500

    Debugging regex in contact form.

--- scripts/contactUs.js
+++ scripts/contactUs.js
@@ -1,103 +1,90 @@
 //Hamburger Menu//
 
 /**Contact Form */
-const userName = document.getElementById("userName").value;
-const subject = document.getElementById("subject").value;
-const phone = document.getElementById("phone").value;
-const email = document.getElementById("email").value;
-const message = document.getElementById("message").value;
+function validate(){
 
-  
-// Can only contain letters a-z in lowercase
-function isValiduserName(userName) {
-    return /^[a-z]+$/.test(userName);
+    var userName = document.getElementById("userName").value;
+    var subject = document.getElementById("subject").value;
+    var phone = document.getElementById("phone").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("messgae").value;
+    var errorMessage = document.getElementById("errorMessage");
+
+    errorMessage.style.padding = "25px";  
+
+
+    // Name can only contain letters a-z in lowercase
+    function isValiduserName(inputText){
+      var nameformat = /^[a-z]+$/;
+      if(inputText.value.match(nameformat))
+      {
+        return true;
+      }
+      else {
+        text = "Your name can only contain letters a-z";
+        error_message.innerHTML = text;
+        return false;
+      }
+    } 
+
+    //Subject can only contain letters a-z in lowercase
+    function isValidSubject(inputText){
+        var subjectformat = /^[a-z]+$/;
+        if(inputText.value.match(subjectformat))
+        {
+          return true;
+        }
+        else {
+          text = "The subject can only contain letters a-z";
+          error_message.innerHTML = text;
+          return false;
+        }
+      }
+
+    // The telephone number must be in the format of (555) 555-5555
+    function isValidPhone(inputText){
+        var phoneformat = /^\D*\d{3}\D*\d{3}\D*\d{4}\D*$/;
+        if(inputText.value.match(phoneformat))
+        {
+          return true;
+        }
+        else {
+          text = "You must enter a telephone number in the format of (555) 555-5555";
+          error_message.innerHTML = text;
+          return false;
+        }
+      }
+
+    //function formatPhone(text){
+     //   const regex = /^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*$/;
+      //  return text.replace(regex, '($1) $2-$3');
+   // }
+
+    // Must be a valid email address
+    function isValidEmail(inputText){
+        var emailformat = /^[^@]+@[^@.]+\.[a-z]+$/i;
+        if(inputText.value.match(emailformat))
+        {
+          return true;
+        }
+        else {
+          text = "You must enter a valid email address";
+          error_message.innerHTML = text;
+          return false;
+        }
+      }
+
+    // Must contain more than 10 characters
+    function isValidMessage(inputText){
+        var messagelength = /^.{10,}$/;
+        if(inputText.value.match(messagelength))
+        {
+          return true;
+        }
+        else {
+          text = "Please Enter More Than 10 Characters in Your Message";
+          error_message.innerHTML = text;
+          return false;
+        }
+      }
 }
-
-// Can only contain letters a-z in lowercase
-function isValidSubject(subject) {
-    return /^[a-z]+$/.test(subject);
-}
-
-
-// The telephone number must be in the format of (555) 555-5555
-function isValidPhone(phone) {
-    return /^\D*\d{3}\D*\d{3}\D*\d{4}\D*$/.test(phone);
-}
-
-function formatPhone(text) {
-    const regex = /^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*$/;
-    return text.replace(regex, '($1) $2-$3');
-}
-
-// Must be a valid email address
-function isValidEmail(email) {
-    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
-}
-
-// Can only contain letters a-z in lowercase
-function isValidMessage(message) {
-    return /^[a-z]+$/.test(message);
-}
-
-/** Error Message **/
-
-function showOrHideTip(show, element) {
-  // show element when show is true, hide when false
-  if (show) {
-    element.style.display = "inherit";
-  } else {
-    element.style.display = "none";
-  }
-}
-
-function createListener(validator) {
-  return e => {
-    const text = e.target.value;
-    const valid = validator(text);
-    const showTip = text !== "" && !valid;
-    const tooltip = e.target.nextElementSibling;
-    showOrHideTip(showTip, tooltip);
-  };
-}
-
-userName.addEventListener("input", createListener(isValiduserName));
-
-subject.addEventListener("input", createListener(isValidSubject));
-
-phone.addEventListener("input", createListener(isValidPhone));
-
-phoneInput.addEventListener("blur", e => {
-    e.target.value = formatPhone(e.target.value);
-});
-
-email.addEventListener("input", createListener(isValidEmail));
-
-message.addEventListener("input", createListener(isValidMessage));
-
-alert("Your contact form was submitted successfully! We will respond as soon as possible!");
-return true;
-
-
-
-/**
-    error_message.style.padding = "10px";
-  
-    var text;
-    if(name.length < 5){
-      text = "Please Enter a Valid Name";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(email.indexOf("@") == -1 || email.length < 6){
-      text = "Please Enter a Valid Email";
-      error_message.innerHTML = text;
-      return false;
-    }
-    if(message.length <= 10){
-      text = "Please Enter More Than 10 Characters in Your Message";
-      error_message.innerHTML = text;
-      return false;
-    }
-    alert("Your contact form was submitted successfully! We will respond as soon as possible!");
-    return true;
-  }**/
